37ff1f18b1aa9944d9c2a81b3df56cb2f7afa93c
yperdoom <yperdoom@gmail.com>
2023-01-29 20:34:04 -0300

    fix client model

--- config/database/models/client.js
+++ config/database/models/client.js
@@ -6,10 +6,10 @@ module.exports.createTable = async () => {
   const create = await client.query(`
     CREATE TABLE IF NOT EXISTS clients (
       client_id int NOT NULL GENERATED ALWAYS AS IDENTITY ( CYCLE INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 999999999 CACHE 1 ),
-      age varchar(50) NOT NULL,
-      height varchar(40) NOT NULL,
-      weight varchar(20) NOT NULL,
-      fat_percentage varchar(10) NOT NULL,
+      age integer NOT NULL,
+      height integer NOT NULL,
+      weight double NOT NULL,
+      fat_percentage double NOT NULL,
       sex varchar(15) NOT NULL,
       user_id int NOT NULL,
       created_at timestamp NOT NULL,
