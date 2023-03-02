6894e8beb42361ca2cf5f8784e2bab6b04d9fec1
yperdoom <yperdoom@gmail.com>
2023-03-02 08:24:09 -0300

    fix: AG repar functions

--- src/services/geneticAlgorithm/evaluateGeneration.js
+++ src/services/geneticAlgorithm/evaluateGeneration.js
@@ -1,35 +1,39 @@
 require('dotenv/config')
-const { SIZE_POPULATION } = process.env
+const { EXPECTED_EVALUATION } = process.env
 
+// Função que irá avaliar a geração de cromossomos informada
 module.exports = (foods, generation) => {
+  // Declara um array que representa a evolução da população
   const evaluation = []
 
-  generation.forEach((chromosome, index) => {
+  // Percorre a geração iterando por cada cromossomo presente nela
+  generation.forEach((individual, index) => {
+    // Variável que será usada para a soma de porcentagens
     let percentageSum = 0
 
-    for (let foodIterator = 0; foodIterator < Math.floor(SIZE_POPULATION); foodIterator++) {
-      evaluation[index][foodIterator].diference = (chromosome[foodIterator] * 100) / foods[foodIterator].quantity
+    // Percorre por todos os alimentos, iterando por cada um deles
+    for (let foodIterator = 0; foodIterator < foods.length; foodIterator++) {
+      // Atribui a diferença de porcentagem
+      evaluation[index][foodIterator].diference = (individual[foodIterator] * 100) / foods[foodIterator].quantity
 
+      // Soma a diferença de porcentagem
       percentageSum += evaluation[index][foodIterator].diference
     }
 
-    const percentageOfChromosome = percentageSum / Math.floor(SIZE_POPULATION)
+    // Adiciona a média de diferença em porcentagem ao indivíduo
+    individual.fitness = (percentageSum / foods.length)
 
-    if (Math.abs(percentageOfChromosome) >= 75) {
+    // Verifica se o individuo bate com a avaliação que se espera
+    if (Math.abs(individual.fitness) >= EXPECTED_EVALUATION) {
       return {
         success: true,
-        chromosome,
+        individual,
         evaluation: evaluation[index]
       }
     }
   })
 
   return {
-    success: false,
-    evaluation
+    success: false
   }
 }
-
-// evaluation[foodIterator].lipid = (foods[foodIterator].lipid * evaluation[foodIterator].diference) / 100
-// evaluation[foodIterator].protein = (foods[foodIterator].protein * evaluation[foodIterator].diference) / 100
-// evaluation[foodIterator].carbohydrate = (foods[foodIterator].carbohydrate * evaluation[foodIterator].diference) / 100
--- src/services/geneticAlgorithm/generateGeneration.js
+++ src/services/geneticAlgorithm/generateGeneration.js
@@ -1,18 +1,33 @@
 require('dotenv/config')
-const { SIZE_POPULATION, MAX_CHROMOSOME_SIZE } = process.env
+const { SIZE_GENERATION, MAX_CHROMOSOME_SIZE } = process.env
 
+// Função que gera uma geração de indivíduos
 module.exports = (foods) => {
-  const population = []
+  // Declara a geração inicial de indivíduos
+  const generation = []
 
-  for (let populationIterator = 0; populationIterator < Math.floor(SIZE_POPULATION); populationIterator++) {
-    const chromosome = []
-
-    for (let foodIterator = 0; foodIterator < foods.length; foodIterator++) {
-      const random = Math.floor(Math.random() * MAX_CHROMOSOME_SIZE)
-      chromosome.push(random)
+  // Inicia a contrução da geração até o tamanho máximo da geração declarado no env
+  for (let generationIterator = 0; generationIterator < Math.floor(SIZE_GENERATION); generationIterator++) {
+    // Declara o objeto do indivíduo atual
+    const individual = {
+      chromosome: [], // Array de inteiros que representa a quantidade (peso, porção ou litros) dos alimentos
+      fitness: 0.00, // Campo que indica a nota de aptidão do indivíduo
+      fitnessPercent: 0.00, // Porcentagem que representa o quanto este indivíduo é apto para reproduzir
+      rouletteRange: [0, 0] // Faixa da fatia de pizza (em porcentagem aplicado na roleta russa) em que representa a chance desse indivíduo ser selecionado
     }
-    population.push(chromosome)
+
+    // Percorre iterando no indivíduo até a quantidade máxima de comidas informada
+    for (let foodIterator = 0; foodIterator < foods.length; foodIterator++) {
+      // Gera um número aleatório entre 0 e o tamanho máximo do cromossomo declarado no env
+      const random = Math.floor(Math.random() * MAX_CHROMOSOME_SIZE)
+
+      // adiciona o número gerado no cromossomo do indivíduo atual
+      individual.chromosome.push(random)
+    }
+    // Adiciona o indivíduo atual na geração
+    generation.push(individual)
   }
 
-  return population
+  // Retorna a geração gerada para a função controladora
+  return generation
 }
