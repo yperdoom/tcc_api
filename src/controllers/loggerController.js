a0a0a30056c374b73ea90350cb1ee181ffe98c23
yperdoom <yperdoom@gmail.com>
2023-03-04 11:31:37 -0300

    fix: Add multiLevels of loggers

--- src/controllers/loggerController.js
+++ src/controllers/loggerController.js
@@ -1,18 +1,49 @@
 
-module.exports.log = (message, type, body, local) => {
-  if (message) {
-    console.log(message)
-  }
+module.exports = ({
+  log: (message, type, body, local) => {
+    const blue = '\u001b[34m'
+    console.log(blue + 'log instance =|')
 
-  if (type) {
-    console.log(type)
-  }
+    if (message) {
+      console.log(message)
+    }
 
-  if (body) {
-    console.log(body)
-  }
+    if (type) {
+      console.log(type)
+    }
 
-  if (local) {
-    console.log(local)
+    if (body) {
+      console.log(body)
+    }
+
+    if (local) {
+      console.log(local)
+    }
+
+    const reset = '\u001b[0m'
+    console.log(reset + 'end log instance =|')
+  },
+  error: (message, type, body, local) => {
+    const red = '\u001b[31m'
+    console.log(red + 'error instance =|')
+
+    if (message) {
+      console.log(message)
+    }
+
+    if (type) {
+      console.log(type)
+    }
+
+    if (body) {
+      console.log(body)
+    }
+
+    if (local) {
+      console.log(local)
+    }
+
+    const reset = '\u001b[0m'
+    console.log(reset + 'end error instance =|')
   }
-}
+})
