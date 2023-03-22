8e9eb2ee508bbd16f4a75a93ba0266ba644b5ebe
yperdoom <yperdoom@gmail.com>
2023-03-22 00:38:06 -0300

    remove return to init tables

--- config/database/postgres/models/Manager.js
+++ config/database/postgres/models/Manager.js
@@ -3,7 +3,7 @@ const database = require('../pgConnection')
 module.exports.createTable = async () => {
   const client = await database.connect()
 
-  const create = await client.query(`
+  await client.query(`
     CREATE TABLE IF NOT EXISTS managers (
       manager_id int NOT NULL GENERATED ALWAYS AS IDENTITY ( CYCLE INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 999999999 CACHE 1 ),
       document varchar(15),
@@ -16,6 +16,4 @@ module.exports.createTable = async () => {
   )
   client.release()
   await database.close()
-
-  return create
 }
