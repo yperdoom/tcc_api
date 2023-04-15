b8d1a22755cb4a6a0bf54614bef0385c7c2ab3c4
yperdoom <yperdoom@gmail.com>
2023-04-15 13:44:16 -0300

    feat: switch to work binary numbers

--- src/services/geneticAlgorithm/crossoverProcess.js
+++ src/services/geneticAlgorithm/crossoverProcess.js
@@ -1,27 +1,71 @@
 require('dotenv/config')
-const { CROSSOVER_RATE } = process.env
+const { CROSSOVER_RATE, MAX_CHROMOSOME_SIZE } = process.env
+const matchSize = (MAX_CHROMOSOME_SIZE >>> 1).toString(2).length
 
 module.exports = async (fatherChromosome, motherChromosome) => {
-  const lengthOfChromosome = (fatherChromosome.chromosome.length + motherChromosome.chromosome.length) / 2
-  const fatherOffspring = fatherChromosome
-  const motherOffspring = motherChromosome
-  const fatherBuffer = []
-  const motherBuffer = []
+  const fatherBinary = _arrayIntegerToBinary(fatherChromosome.chromosome)
+  const motherBinary = _arrayIntegerToBinary(motherChromosome.chromosome)
 
-  fatherBuffer.push(...fatherChromosome.chromosome)
-  motherBuffer.push(...motherChromosome.chromosome)
+  const fatherOffspring = fatherBinary
+  const motherOffspring = motherBinary
+  const fatherBuffer = [...fatherBinary]
+  const motherBuffer = [...motherBinary]
 
   if ((Math.floor(Math.random() * 100)) <= CROSSOVER_RATE) {
-    const cutoff = Math.floor(Math.random() * lengthOfChromosome)
+    const cuttingPlace = Math.floor(Math.random() * (fatherChromosome.chromosome.length - 1))
 
-    for (let i = cutoff; i < lengthOfChromosome; i++) {
-      motherOffspring.chromosome[i] = fatherBuffer[i]
-      fatherOffspring.chromosome[i] = motherBuffer[i]
+    for (let i = cuttingPlace; i < (fatherChromosome.chromosome.length); i++) {
+      const cutoff = Math.floor(Math.random() * (matchSize - 1))
+
+      fatherOffspring[i] = ''
+      motherOffspring[i] = ''
+
+      for (let j = 0; j < matchSize; j++) {
+        if (j >= cutoff) {
+          fatherOffspring[i] += motherBuffer[i][j]
+          motherOffspring[i] += fatherBuffer[i][j]
+        } else {
+          fatherOffspring[i] += fatherBuffer[i][j]
+          motherOffspring[i] += motherBuffer[i][j]
+        }
+      }
     }
   }
 
   return {
-    fatherSon: fatherOffspring,
-    motherSon: motherOffspring
+    fatherSon: {
+      chromosome: _arrayBinaryToInteger(fatherOffspring)
+    },
+    motherSon: {
+      chromosome: _arrayBinaryToInteger(motherOffspring)
+    }
   }
+}
+
+const _arrayIntegerToBinary = (array) => {
+  const binaryArray = []
+
+  for (let i = 0; i < array.length; i++) {
+    let toBinary = (array[i] >>> 1).toString(2)
+
+    if (toBinary.length < matchSize) {
+      for (let j = toBinary.length; j < matchSize; j++) {
+        toBinary = '0' + toBinary
+      }
+    }
+
+    binaryArray.push(toBinary)
+  }
+
+  return binaryArray
+}
+
+const _arrayBinaryToInteger = (array) => {
+  const integerArray = []
+
+  for (let i = 0; i < array.length; i++) {
+    integerArray.push(parseInt(array[i], 2))
+  }
+
+  return integerArray
 }
