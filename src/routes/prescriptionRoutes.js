bb203edae8b2335d3fb51887514cbb9f533afebe
yperdoom <yperdoom@gmail.com>
2023-04-30 09:34:46 -0300

    init get prescriptions route

--- src/routes/prescriptionRoutes.js
+++ src/routes/prescriptionRoutes.js
@@ -3,6 +3,8 @@ const prescriptionController = require('../controllers/prescriptionController')
 module.exports = (api) => {
   // api.get('/prescription', prescriptionController.getAll)
 
+  api.get('/prescription/:user_id', prescriptionController.getByUser)
+
   // api.get('/prescription/:prescription_id', prescriptionController.getprescription)
 
   api.post('/prescription', prescriptionController.create)
