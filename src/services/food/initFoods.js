2df4660d8060f7a20e1ec1a4d0f0b23edefff66c
yperdoom <yperdoom@gmail.com>
2023-05-13 20:37:04 -0300

    sdaiuhfkwue

--- src/services/food/initFoods.js
+++ src/services/food/initFoods.js
@@ -3,20 +3,31 @@ const time = require('../factory/getTimeNow')
 const tacoFoods = require('../../../TACO.json')
 
 module.exports = async () => {
-  for (let i = 0; i < tacoFoods.length; i++) {
-    const payload = {
-      name: tacoFoods[i].description,
-      description: tacoFoods[i].description,
-      type: tacoFoods[i].category,
-      weight: 100,
-      calorie: tacoFoods[i].energy_kcal,
-      protein: tacoFoods[i].protein_g,
-      lipid: tacoFoods[i].lipid_g,
-      carbohydrate: tacoFoods[i].carbohydrate_g,
-      created_at: time.now(),
-      updated_at: time.now()
-    }
+  await createFood({
+    name: tacoFoods[0].description,
+    description: tacoFoods[0].description,
+    type: tacoFoods[0].category,
+    weight: 100,
+    calorie: tacoFoods[0].energy_kcal,
+    protein: tacoFoods[0].protein_g,
+    lipid: tacoFoods[0].lipid_g,
+    carbohydrate: tacoFoods[0].carbohydrate_g,
+    created_at: time.now(),
+    updated_at: time.now()
+  })
 
-    await createFood(payload)
-  }
+  // for (let i = 0; i < tacoFoods.length; i++) {
+  //   await createFood({
+  //     name: tacoFoods[i].description,
+  //     description: tacoFoods[i].description,
+  //     type: tacoFoods[i].category,
+  //     weight: 100,
+  //     calorie: tacoFoods[i].energy_kcal,
+  //     protein: tacoFoods[i].protein_g,
+  //     lipid: tacoFoods[i].lipid_g,
+  //     carbohydrate: tacoFoods[i].carbohydrate_g,
+  //     created_at: time.now(),
+  //     updated_at: time.now()
+  //   })
+  // }
 }
