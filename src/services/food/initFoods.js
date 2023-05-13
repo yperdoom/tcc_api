a7f92f58b24681ceda687e118740536f9240d43e
yperdoom <yperdoom@gmail.com>
2023-05-13 20:17:27 -0300

    finish taco foods add on init

--- src/services/food/initFoods.js
+++ src/services/food/initFoods.js
@@ -3,28 +3,24 @@ const database = require('../../../config/database/postgres/pgConnection')
 const time = require('../factory/getTimeNow')
 const tacoFoods = require('../../../TACO.json')
 
-const testFunction = () => {
-  const payload = {
-    name: tacoFoods[0].description,
-    description: tacoFoods[0].description,
-    type: tacoFoods[0].category,
-    weight: 100,
-    calorie: tacoFoods[0].energy_kcal,
-    protein: tacoFoods[0].protein_g,
-    lipid: tacoFoods[0].lipid_g,
-    carbohydrate: tacoFoods[0].carbohydrate_g,
-    updated_at: time.now(),
-    created_at: time.now()
+module.exports = async () => {
+  for (let i = 0; i < tacoFoods.length; i++) {
+    const payload = {
+      name: tacoFoods[i].description,
+      description: tacoFoods[i].description,
+      type: tacoFoods[i].category,
+      weight: 100,
+      calorie: tacoFoods[i].energy_kcal,
+      protein: tacoFoods[i].protein_g,
+      lipid: tacoFoods[i].lipid_g,
+      carbohydrate: tacoFoods[i].carbohydrate_g,
+      updated_at: time.now(),
+      created_at: time.now()
+    }
+
+    await connectAndUpdate(payload)
   }
-
-  console.log(tacoFoods[0])
-  // for (let i = 0; i < 10; i++) {
-  //   console.log(i)
-  // }
-  // await connectAndUpdate(payload)
 }
-
-testFunction()
 
 const connectAndUpdate = async (payload) => {
   const client = await database.connect()
