6a2572db2e66170d286ce79f0cf6d76f849f9b04
yperdoom <yperdoom@gmail.com>
2023-05-13 20:33:06 -0300

    fiisa

--- src/services/food/initFoods.js
+++ src/services/food/initFoods.js
@@ -1,5 +1,4 @@
-const Logger = require('../../controllers/loggerController')
-const database = require('../../../config/database/postgres/pgConnection')
+const createFood = require('../food/createFood')
 const time = require('../factory/getTimeNow')
 const tacoFoods = require('../../../TACO.json')
 
@@ -14,64 +13,10 @@ module.exports = async () => {
       protein: tacoFoods[i].protein_g,
       lipid: tacoFoods[i].lipid_g,
       carbohydrate: tacoFoods[i].carbohydrate_g,
-      updated_at: time.now(),
-      created_at: time.now()
+      created_at: time.now(),
+      updated_at: time.now()
     }
 
-    await connectAndUpdate(payload)
+    await createFood(payload)
   }
-}
-
-const connectAndUpdate = async (payload) => {
-  const client = await database.connect()
-  if (!client) {
-    return null
-  }
-
-  try {
-    const query = {
-      text: `INSERT INTO 
-        foods(
-          name,
-          description,
-          type,
-          color,
-          weight,
-          portion,
-          mililiter,
-          calorie,
-          protein,
-          lipid,
-          carbohydrate,
-          created_at,
-          updated_at
-        )
-        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)`,
-      values: [
-        payload.name,
-        payload.description,
-        payload.type,
-        payload.color,
-        payload.weight,
-        payload.portion,
-        payload.mililiter,
-        payload.calorie,
-        payload.protein,
-        payload.lipid,
-        payload.carbohydrate,
-        payload.created_at,
-        payload.updated_at
-      ]
-    }
-
-    await client.query(query)
-  } catch (error) {
-    Logger.error({
-      error: error.error,
-      type: 'database-error',
-      local: 'postgre-init-foods-service'
-    })
-  }
-  client.release()
-  await database.close()
 }
