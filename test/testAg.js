364925c7e5180fe97e6aa55ff6f893d847586270
Pedro Henrique Antunes Pinto <pedro.pinto@amo.delivery>
2023-06-03 09:52:24 -0300

    dsakjdakjwalf kjw

--- test/testAg.js
+++ test/testAg.js
@@ -1,6 +1,6 @@
 const agController = require('../src/controllers/agController')
 
 const foods = require('./mocks/foodsMock.json')
-const meal = require('./mocks/mealMock2.json')
+const meal = require('./mocks/meal_validation.json')
 
 agController.newAdapter(foods, meal)
